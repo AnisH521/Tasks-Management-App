0c4125984404fecdd4195158b3579f56a8ea499e
AnisH <anishnaskar99@gmail.com>
2025-09-05 12:55:38 +0530

    style: standardize quotes and update SameSite attribute in createJWT function

--- api/util/createJWT.js
+++ api/util/createJWT.js
@@ -1,14 +1,14 @@
-import jwt from 'jsonwebtoken';
-import config from 'config';
+import jwt from "jsonwebtoken";
+import config from "config";
 
 export const createJWT = (res, userId) => {
   const token = jwt.sign({ userId }, config.get("jwt.access_token_secret"), {
-    expiresIn: '1d',
+    expiresIn: "1d",
   });
-  res.cookie('token', token, { 
+  res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-    sameSite: 'strict', // Helps prevent CSRF attacks
-    maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds 
+    secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+    sameSite: "none", // Helps prevent CSRF attacks
+    maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
   });
 };
